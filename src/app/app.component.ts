6b6198d9b4d6efedef4d3eabb339b824a3cef5dc
half-winter-moon <han16064@byui.edu>
2022-04-28 10:45:14 -0600

    cms project, contacts portion complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
   selector: 'cms-root',
+  // every component MUST have a template, or templateURL
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
