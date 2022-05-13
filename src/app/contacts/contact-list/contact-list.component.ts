9c1a6833487564b69e1a2855feac387910184a7d
half-winter-moon <han16064@byui.edu>
2022-05-13 10:39:43 -0600

    week 4 assignment complete with dropdown and documents

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -6,7 +6,6 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent implements OnInit {
-  // constructor() {};
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
   contacts: Contact[] = [
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    new Document('100', 'Jon Doe', 'Message', '#', null),
+    new Document('101', 'Jane Doe', 'Message', '#', null),
+    new Document('102', 'Jonny Doe', 'Message', '#', null),
+    new Document('103', 'Jonathan Doe', 'Message', '#', null),
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
-
 }
